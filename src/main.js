1b6a7770a0fcbbe83b64a7333788a82088129d57
unknown <abuzcoder@gmail.com>
2021-10-14 15:53:30 +0500

    update

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import "./assets/tailwind.css";
 import "./font/stylesheet.css";
-createApp(App)
-  .mount("#app")
-  .use(AOS.init());
+const app = createApp(App);
+app.mount("#app");
+app.use(AOS.init());
